0fa630bcd63b48df754b6b68beae463112d2b523
KingSoulRips <141964455+KingSoulRips@users.noreply.github.com>
2023-08-11 05:19:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,39 +2,27 @@ let heads = 0;
 let tails = 0;
 let coin = document.querySelector(".coin");
 let flipBtn = document.querySelector("#flip-button");
-let resetBtn = document.querySelector("#reset-button");
-
+let resultText = document.querySelector("#result"); // Select the <p> element to display result
 
 flipBtn.addEventListener("click", () => {
     let i = Math.floor(Math.random() * 2);
     coin.style.animation = "none";
-    if(i){
-        setTimeout(function(){
+    
+    if (i) {
+        setTimeout(function () {
             coin.style.animation = "spin-heads 3s forwards";
+            setTimeout(function () {
+                resultText.textContent = "HEADS"; // Display result text after 3 seconds
+            }, 3000); // 3000 milliseconds = 3 seconds
         }, 100);
         heads++;
-    }
-    else{
-        setTimeout(function(){
+    } else {
+        setTimeout(function () {
             coin.style.animation = "spin-tails 3s forwards";
+            setTimeout(function () {
+                resultText.textContent = "TAILS"; // Display result text after 3 seconds
+            }, 3000); // 3000 milliseconds = 3 seconds
         }, 100);
         tails++;
     }
-    
-
 });
-
-
-flipButton.addEventListener('click', () => {
-    coin.classList.toggle('flipped');
-    isHeadsVisible = !isHeadsVisible; // Toggle the side
-
-    if (isHeadsVisible) {
-        headsDiv.textContent = 'Heads'; // Update text content
-        tailsDiv.textContent = ''; // Clear text content
-    } else {
-        tailsDiv.textContent = 'Tails'; // Update text content
-        headsDiv.textContent = ''; // Clear text content
-    }
-});
-
